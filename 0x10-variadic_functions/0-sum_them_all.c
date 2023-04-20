4b53c09ff9e799189cace60c0ca3bd8f712d02a8
byahyal01 <byahya882@gmail.com>
2023-04-20 06:59:31 -0700

    varidic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,20 @@
-#include "variadic_functions.h"
-
+#include <stdarg.h>
 /**
- * sum_them_all - calculates the sum of all its parameters
- * @n: number of arguments passed to the function
- *
- * Return: the resulting sum
+ * sum_them_all - sums all arguments
+ * @n: number of arguments
+ * Return: sum of arguments
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
 	int sum = 0;
-	va_list list;
+	unsigned int i;
+	va_list args;
 
-	va_start(list, n);
-
+	if (n == 0)
+		return (0);
+	va_start(args, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(list, int);
-
-	va_end(list);
-
+		sum += va_arg(args, int);
+	va_end(args);
 	return (sum);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,46 +1,29 @@
-#include "variadic_functions.h"
-	#include <stdio.h>
-	#include <stdarg.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_strings - prints strings
+ * @separator: string to be printed between strings
+ * @n: number of strings passed to the function
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *str;
 
-
-	/**
-	 * print_strings - Prints strings, followed by a new line.
-	 * @separator: The string to be printed between strings.
-	 * @n: The number of strings passed to the function.
-	 * @...: A variable number of strings to be printed.
-	 *
-	 * Description: If separator is NULL, it is not printed.
-	 *              If one of the strings if NULL, (nil) is printed instead.
-	 */
-	void print_strings(const char *separator, const unsigned int n, ...)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		va_list strings;
-		char *str;
-		unsigned int index;
-
-
-		va_start(strings, n);
-
-
-		for (index = 0; index < n; index++)
-		{
-			str = va_arg(strings, char *);
-
-
-			if (str == NULL)
-				printf("(nil)");
-			else
-				printf("%s", str);
-
-
-			if (index != (n - 1) && separator != NULL)
-				printf("%s", separator);
-		}
-
-
-		printf("\n");
-
-
-		va_end(strings);
+		str = va_arg(args, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
-}
+	printf("\n");
+	va_end(args);
+}}
